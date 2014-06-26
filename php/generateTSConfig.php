b65862ff1f107ae06a5c7ad8078de22c2f39a1e6
Gareth Dunstone <stormaes@Taranis-2.local>
2014-06-26 17:18:45 +1000

    Changed to using DateTime for timstamps
    OO method of working out and formatting the correct dates.

--- php/generateTSConfig.php
+++ php/generateTSConfig.php
@@ -4,6 +4,7 @@ session_start();
 include "template.php";
 include "globals.php";
 
+date_default_timezone_set('UTC');
 // read the xml template as an xml string into a SimpleXMLElement so that we can play around with it.
 
 $expire=time()+60*60*24*30;
@@ -81,24 +82,11 @@ $number_of_streams = count($streams);
 		$xml = new SimpleXMLElement($xmlstr);
 		// setting the config name to the expt id (assuming all the config files)
 		$xml->globals['config_id'] = $expts_decoded[0]->experiments[$experiment_index]->expt_id;
+		$start_date_time = new DateTime($expts_decoded[0]->experiments[$experiment_index]->start_date.$expts_decoded[0]->experiments[$experiment_index]->start_time);
+		$end_date_time = new DateTime($expts_decoded[0]->experiments[$experiment_index]->end_date.$expts_decoded[0]->experiments[$experiment_index]->end_time);
 
-		//should functionalise this date string screwery.
-		$full_backwards_start_date = $expts_decoded[0]->experiments[$experiment_index]->start_date;
-		$start_day = substr($full_backwards_start_date, 8, 2);
-		$start_month = substr($full_backwards_start_date, 5, 2);
-		$start_year = substr($full_backwards_start_date, 0 , 4);
-
-		$start_time = $expts_decoded[0]->experiments[$experiment_index]->start_time;
-
-		$full_backwards_end_date = $expts_decoded[0]->experiments[$experiment_index]->end_date;
-		$end_day = substr($full_backwards_end_date, 8, 2);
-		$end_month = substr($full_backwards_end_date, 5, 2);
-		$end_year = substr($full_backwards_end_date, 0 , 4);
-		$end_time = $expts_decoded[0]->experiments[$experiment_index]->end_time;
-
-		// more date string concat screwery setting up the dates for the globals
-		$xml->globals['date_start'] = "$start_month"."/"."$start_day"."/"."$start_year"." "."$start_time"." PM";
-		$xml->globals['date_end'] = "$end_month"."/"."$end_day"."/"."$end_year"." "."$end_time"." PM";
+		$xml->globals['date_start'] = date_format($start_date_time, 'm/d/Y H:i:s A');
+		$xml->globals['date_end'] = date_format($end_date_time, 'm/d/Y H:i:s A');
 		// iterating through the first experiment and then the list of timestreams
 		// change this to POST/GET user selection later
 		for ($check=0; $check < count($streams); $check++) { 
