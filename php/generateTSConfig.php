eefad368ba739bfccd63442394454fa3e22cf2bf
gdunstone <freedom.2.the.leetle.people@gmail.com>
2014-10-21 18:44:08 +1100

    added span to jsons and generator
    Now individual experiments can have their own starting timespan.

--- php/generateTSConfig.php
+++ php/generateTSConfig.php
@@ -99,6 +99,7 @@ $number_of_streams = count($streams);
 		// setting the config name to the expt id (assuming all the config files)
 		$xml->globals['config_id'] = $expts_decoded[0]->experiments[$experiment_index]->expt_id;
 		$xml->globals['background_color'] = "0x".$bg_color;
+		$xml->globals['timespan_days_init'] = $expts_decoded[0]->experiments[$experiment_index]->timespan_days
 		$start_date_time = new DateTime($expts_decoded[0]->experiments[$experiment_index]->start_date.$expts_decoded[0]->experiments[$experiment_index]->start_time);
 		$now = new DateTime('now');
 		$end_date_time = new DateTime($expts_decoded[0]->experiments[$experiment_index]->end_date.$expts_decoded[0]->experiments[$experiment_index]->end_time);
